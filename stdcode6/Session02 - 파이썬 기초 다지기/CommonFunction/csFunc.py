fdaca5de3be36bfa5de7c6a76aee46898ffed27f
JinWoo <bjw8518@gmail.com>
2023-03-17 18:35:36 +0900

    cm
    cm

--- stdcode6/Session02 - 파이썬 기초 다지기/CommonFunction/csFunc.py
+++ stdcode6/Session02 - 파이썬 기초 다지기/CommonFunction/csFunc.py
@@ -5,18 +5,18 @@ def minMaxAvg(testList):
     return: 리스트내 값에서 최소 최댓값을 뺀 평균을 반환한다
     """
 
-#testList=[2,5,3,7,8,4,1,6,9]
-minValue = min(testList)
-maxValue = max(testList)
-print("최대값{} 최솟값{}".format(minValue,maxValue) )
+    #testList=[2,5,3,7,8,4,1,6,9]
+    minValue = min(testList)
+    maxValue = max(testList)
+    print("최대값{} 최솟값{}".format(minValue,maxValue) )
 
-testList.remove(minValue)
-testList.remove(maxValue)
+    testList.remove(minValue)
+    testList.remove(maxValue)
 
-average = 0
+    average = 0
 
-if len(testList) !=0:
-    average =sum(testList)/len(testList)
-else :
-    pass
-return average
+    if len(testList) !=0:
+        average =sum(testList)/len(testList)
+    else :
+        pass
+    return average
